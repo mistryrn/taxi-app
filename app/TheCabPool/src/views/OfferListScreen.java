06a23b4fbd4592f58b68b12942536d4ff1d06e59
Jordan Floyd <floyd@live.ca>
2014-12-03 13:01:32 -0500

    Accepting works, you can now get through an entirely positive scenario.

--- app/TheCabPool/src/views/OfferListScreen.java
+++ app/TheCabPool/src/views/OfferListScreen.java
@@ -10,13 +10,17 @@ import java.util.TimerTask;
 
 import library.SphericalUtil;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import AsyncTasks.DispatcherTask;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -25,10 +29,12 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Checkable;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -48,23 +54,102 @@ public class OfferListScreen extends FragmentActivity{
 		private Button btnAccept;
 		private Button btnDecline;
 		private ListView lstRequests;
-	  
+		private ArrayAdapter<String> listAdapter;
+		private static JSONObject[] requestList = new JSONObject[10];
+		private String previousRequest;
+		private TextView lblSelectedChoice;
+		private static String selectedUser;
+		private static boolean refreshFlag;
+		private static String cost;
+		
 	  /** Called when the activity is first created. */
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_offer_list_screen);
 	    
+	    lblSelectedChoice = (TextView) findViewById(R.id.lblOfferListSelectedChoice);
 	    btnAccept = (Button) findViewById(R.id.btnOfferListScreenAccept);
 	    btnDecline = (Button) findViewById(R.id.btnOfferListScreenDecline);
+	    ShareController controller = new ShareController(this);
+	    btnAccept.setOnClickListener(controller);
+	    btnDecline.setOnClickListener(controller);
+	    
 	    lstRequests = (ListView) findViewById(R.id.lstOfferListScreenRequestsList);
+	    listAdapter = new ArrayAdapter<String>(this, R.layout.list_item_offer, new ArrayList<String>());
+	    lstRequests.setAdapter(listAdapter);
+	    lstRequests.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	    refreshFlag = true;
+	    new Timer().schedule(new TimerTask() {
+	        @Override
+	        public void run() {
+	            runOnUiThread(new Runnable() {
+	                public void run() {
+	                	listAdapter.clear();
+	                	StringBuffer request = new StringBuffer();
+	                	try{
+	                		for(int i=0; i<10; i++){
+	                			if(requestList[i] != null){
+	                				request.append("Username/Rating: ");
+	                				request.append(requestList[i].getString("username")+"/"+requestList[i].getString("rating"));
+	                				request.append(", "+requestList[i].getString("cost"));
+	                				request.append(", "+requestList[i].getString("duration"));
+	                				
+
+	                				listAdapter.add(request.toString());
+	                			}
+	                			request.delete(0, request.length());
+	                			}
+	                	    }catch(Exception e){
+	                	    	e.printStackTrace();
+	                	    }
+	                	if(refreshFlag){
+	                	List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+	    				nameValuePairs.add(new BasicNameValuePair("requestType", "retrieveRequests"));
+	    				nameValuePairs.add(new BasicNameValuePair("username", LoginScreen.getLoginData()[0]));
+	    				DispatcherTask retrieveRequests = new DispatcherTask("Share", nameValuePairs);
+	    				retrieveRequests.execute();
+	                	}
+	                }
+	            });
+	        }
+	    }, 3000, 5000);
 	    
 	    
-	   
-		 
+	    lstRequests.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                    long id) {
+            	if(previousRequest!=null) listAdapter.remove(previousRequest);
+            	String request = listAdapter.getItem(position);
+            	lblSelectedChoice.setText("Selected: " + request);
+            	try {
+					selectedUser = requestList[position].getString("username");
+					cost = requestList[position].getString("cost");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+            }
+        });
+	
 	  }
+
+	public static void setRequests(JSONObject[] newRequestList) {
+		requestList = newRequestList;
+	}
 	  
 	
+	public static String getSelectedUser(){
+		return selectedUser;
+	}
+	
+	public static void setRefreshFlag(boolean b){
+		refreshFlag = b;
+	}
+	
+	public static String getCost(){
+		return cost;
+	}
 	  
 	  
 }
